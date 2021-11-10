ec97b2058baf9a47fd47a9bc0dcb4a183df0054f
ellee2 <90704946+ellee2@users.noreply.github.com>
2021-11-10 01:54:18 +0100

    Consegna 4

--- sketch.js
+++ sketch.js
@@ -1,13 +1,74 @@
-function preload() {
-  // put preload code here
+const phrase = "carote olio mandorle zucchero farina cannella vaniglia limone uova sale pomodori panna cioccolato miele pistacchi maizena rum zucca pere";
+const words = phrase.split(" ");
+let buttons;
+let iterator = 0;
+var textfield;
+var output;
+
+function preload(){
+  bg = loadImage('assets/recipe.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  
+  textfield = createInput();
+  textfield.position(50, 650);
+  textfield.changed(newText);
+  output = select('#output');
+  output.position(50, 60);
+  
+
+  words.forEach(word => {
+    createElement("button", word);
+  })
+
+  buttons = selectAll("button");
+
+  buttons.forEach(button => {
+    
+    button.style("position: absolute");
+    button.style('font-size', '25px');
+    button.style('background-color', 'orange');
+    button.style('stroke-color', 'orange');
+    
+  });
+  console.log(buttons);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  background(220);
+ 
+  image(bg, 0, 0, windowWidth, windowHeight);
+  var myText = "Carrot Cake recipe";
+  push();
+  textFont("Pacifico");
+  textAlign(LEFT);
+  textSize(70);
+  fill('orange');
+  text(myText, 50, 80);
+  pop();
+
+  var myText2 = "Write down your list, choose carefully";
+  push();
+  textAlign(LEFT);
+  textSize(15);
+  fill('black');
+  text(myText2, 50, 620);
+  pop();
+  iterator ++;
+  buttons.forEach((button, i) => {
+    let x = noise((iterator + (40 * i)) / 400) * windowWidth;
+    let y = noise((iterator - (40 * i)) / 400) * windowHeight;
+    button.position(x, y);
+});
+}
+
+
+
+
+function newText() {
+
+  createP(textfield.value());
+
 }
